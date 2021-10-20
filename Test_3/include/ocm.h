c0d43910dc5f0c65eca5957ba266293e51875e47
mahdeemushfiquekamal <mmkdhrubo@gmail.com>
2021-10-20 11:13:30 +0600

    Added OCCMS

--- Test_3/include/ocm.h
+++ Test_3/include/ocm.h
@@ -37,7 +37,7 @@ public:
         //POST_REQ(core_.size() == (nh_ << np_), "core must be properly sized");            //for throwing exception
     }
 
-    void addh_val(uint64_t val) {
+    void update_count(uint64_t val) {
         std::vector<CounterType> counts(nh_);
         std::vector<u_int64_t> pos(nh_);
         auto cptr = counts.data();                       //cptr points to the beginning of counts_ vector
@@ -114,15 +114,16 @@ public:
         core_.resize(nh_ << np_);
         //POST_REQ(core_.size() == (nh_ << np_), "core must be properly sized");            //for throwing exception
         collision_.resize(nh_ << np_);
-        for(int i=0;i<collision_.size(); i++) collision_[i] =0;
+        for(int i=0;i<collision_.size(); i++) collision_[i] = 0;
     }
 
     void clear_core(){
         core_.clear();
+        core_.shrink_to_fit();
         core_.resize(nh_ << np_);
     }
 
-    void addh_val(uint64_t val) {                       // update count function
+    void update_count(uint64_t val) {                       // update count function
         std::vector<CounterType> counts(nh_);
         std::vector<u_int64_t> pos(nh_);
         auto cptr = counts.data();                       //cptr points to the beginning of counts_ vector
@@ -132,17 +133,65 @@ public:
             pos[added] = (hv & mask_) + (added << np_);   // exact positions where we will increase the counter by one.
         }
 
-        if(conservative_ == false){
-            int min_collision = std::numeric_limits<int>::max();
-            for(unsigned added = 0; added < nh_; added++){
-                min_collision = std::min(min_collision, collision_[pos[added]]);
+        int min_collision = std::numeric_limits<int>::max();
+        for(unsigned added = 0; added < nh_; added++){
+            min_collision = std::min(min_collision, collision_[pos[added]]);
+        }
+        //std::cout<<"DEBUG "<<"min collision is : "<<min_collision<<std::endl;
+        for(unsigned added = 0; added < nh_; added++){
+            if( collision_[pos[added]] == min_collision) core_[pos[added]]++;
+            else std::cout<<"Collision Deceted while updating"<<std::endl;
+        }
+    }
+
+    void update_count_collision(uint64_t val, int current_round, int total_round){
+        std::vector<CounterType> counts(nh_);
+        std::vector<u_int64_t> pos(nh_);
+        auto cptr = counts.data();                       //cptr points to the beginning of counts_ vector
+        for(unsigned added = 0; added < nh_; added++){
+            CounterType hv = hf_(val, added);
+            cptr[added] = hv;               //counts vector now contains hash values
+            pos[added] = (hv & mask_) + (added << np_);   // exact positions where we will increase the counter by one.
+        }
+
+        int min_collision = std::numeric_limits<int>::max();
+        for(unsigned added = 0; added < nh_; added++){
+            min_collision = std::min(min_collision, collision_[pos[added]]);
+        }
+        //std::cout<<"DEBUG "<<"min collision is : "<<min_collision<<std::endl;
+        if(min_collision < current_round - 1){
+            // # >= 1 cell without collision in prev round
+            CounterType min_count = std::numeric_limits<int>::max();
+            for(unsigned added=0; added< nh_; added++){
+                if(collision_[pos[added]] == min_collision) min_count = std::min(min_count, core_[pos[added]]);
             }
-            //std::cout<<"DEBUG "<<"min collision is : "<<min_collision<<std::endl;
-            for(unsigned added = 0; added < nh_; added++){
-                if( collision_[pos[added]] == min_collision) core_[pos[added]]++;
+
+            for(unsigned added=0; added< nh_; added++){
+                if(collision_[pos[added]] == min_collision){
+                    // c[i,j] = min(C[i,j]+1, min_count)
+                    // Changed code from paper
+                    //core_[pos[added]] = std::min( core_[pos[added]] +1, min_count);
+                    core_[pos[added]] = std::min( core_[pos[added]], min_count) + 1;
+                }
             }
         }
-        else if(conservative_ == true){
+
+        else{
+            // every cell has a collision in the prev round
+            CounterType min_count = std::numeric_limits<int>::max();
+            for(unsigned added=0; added< nh_; added++){
+                min_count = std::min(min_count, core_[pos[added]]);
+            }
+
+            for(unsigned added=0; added< nh_; added++){
+                if(current_round < total_round && core_[pos[added]]){
+                    collision_[pos[added]] = current_round;
+                }
+                // Changed Code from paper
+                //core_[pos[added]] = std::min( core_[pos[added]] + 1, min_count);
+                core_[pos[added]] = std::min( core_[pos[added]], min_count) + 1;
+            }
+
         }
     }
 
@@ -163,7 +212,7 @@ public:
         //std::cout<<"DEBUG "<<"min collision is : "<<min_collision<<std::endl;
 
         if (min_collision >= round-2){
-
+            //std::cout<<"Came to update collision"<<std::endl;
             // find min-count
             CounterType min_count = std::numeric_limits<CounterType>::max();
             for(unsigned added = 0; added < nh_; added++){
@@ -171,10 +220,13 @@ public:
             }
 
             for(unsigned added = 0; added < nh_; added++){
-                if (core_[pos[added]] > min_count) collision_[pos[added]] = round - 1;
+                if (core_[pos[added]] > min_count){
+                    collision_[pos[added]] = round - 1;
+                    std::cout<<"Collision Detected"<<std::endl;
+                }
+
             }
         }
-
     }
 
     CounterType est_count(uint64_t val) const {
@@ -198,7 +250,42 @@ public:
             if( collision_[pos[added]] == min_collision) min_count = core_[pos[added]];
         }
         return min_count;
+    }
+
+    void write_sketch(std::string fname){
+        std::ofstream outputFile(fname);
+        outputFile << np_<<" " << nh_ <<" " << seedseed_<<std::endl;
+        auto cptr = core_.data();
+        for(int i=0;i< nh_ << np_; i++){
+            //outputFile.write((char *) &core_[i], sizeof(CounterType));
+            outputFile<< core_[i]<<" ";
         }
+        outputFile.close();
+        std::cout<<"Write Successful"<<std::endl;
+    }
+
+    void read_sketch(std::string fname){
+        std::ifstream inputFile(fname);
+        inputFile >>np_ >> nh_ >> seedseed_;
+        mask_ = (1ull << np_) - 1;
+        std::cout << np_<<" " << nh_ <<" " << seedseed_<<std::endl;
+        core_.clear();
+        core_.shrink_to_fit();
+        std::cout<<"Core is reinitialized"<<std::endl;
+//
+//        for(std::string line = ""; getline(inputFile, line);){
+//            std::cout<<line<<"\n";
+//        }
+
+        CounterType new_data;
+
+        while(inputFile >> new_data){
+            //auto cptr = core_[]
+            std::cout<< new_data<<std::endl;
+            //core_.push_back(new_data);
+        }
+        std::cout<<"File is read"<<std::endl;
+    }
 };
 
 } }
--- Test_3/main.cpp
+++ Test_3/main.cpp
@@ -7,109 +7,15 @@ int64_t cal(string str_k_mer);
 string sample_kmer="AAACCGGTCTGTTGGGACCACT";  // len = 22
 int counter=0;
 
-template <typename ccmbase_obj> void update_count_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name)
-{
-    std::ifstream input(file);
-    if(!input.good()){cout<<"Can't open"<<endl;}
-
-    std::string line, name, content;
-    while( std::getline( input, line ).good()){ //reading 1 line from input
-
-        if( line.empty() || line[0] == '>' ){
-            if( !name.empty() ){
-                int l=content.length();
-                for(int i=0; i<=l-len_k_mer; i++) //extracting k-mers from the line
-                {
-                    string part = content.substr(i, len_k_mer);
-                    int64_t k_mer = cal(part);
-                    /////////Updating part.
-                    ccmbase_obj_name.addh_val(k_mer);
-                    if(k_mer == cal(sample_kmer))counter++;
-                }
-                name.clear();
-            }
-            if( !line.empty() ){name = line.substr(1);}
-            content.clear();
-        }
-        else if( !name.empty() ){
-            if( line.find(' ') != std::string::npos ){
-                name.clear();
-                content.clear();
-            }
-            else {
-                content += line;
-            }
-        }
-    }
-    if( !name.empty() ){
-        //std::cout << name << " : " << content << std::endl;
-        int l=content.length();
-        for(int i=0; i<=l-len_k_mer; i++)
-        {
-            string part = content.substr(i, len_k_mer);
-            int64_t k_mer = cal(part);
-
-            //// updating part.
-            ccmbase_obj_name.addh_val(k_mer);
-            if(k_mer == cal(sample_kmer))counter++; //manual count of the sample k-mer
-
-        }
-    }
-}
-
-template <typename ccmbase_obj> void update_collision_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name, int round)
-{
-    std::ifstream input(file);
-    if(!input.good()){cout<<"Can't open"<<endl;}
-
-    std::string line, name, content;
-    while( std::getline( input, line ).good()){ //reading 1 line from input
-
-        if( line.empty() || line[0] == '>' ){
-            if( !name.empty() ){
-                int l=content.length();
-                for(int i=0; i<=l-len_k_mer; i++) //extracting k-mers from the line
-                {
-                    string part = content.substr(i, len_k_mer);
-                    int64_t k_mer = cal(part);
-                    /////////Updating part.
-                    ccmbase_obj_name.update_collision(k_mer, round);
-                }
-                name.clear();
-            }
-            if( !line.empty() ){name = line.substr(1);}
-            content.clear();
-        }
-        else if( !name.empty() ){
-            if( line.find(' ') != std::string::npos ){
-                name.clear();
-                content.clear();
-            }
-            else {
-                content += line;
-            }
-        }
-    }
-    if( !name.empty() ){
-        //std::cout << name << " : " << content << std::endl;
-        int l=content.length();
-        for(int i=0; i<=l-len_k_mer; i++)
-        {
-            string part = content.substr(i, len_k_mer);
-            int64_t k_mer = cal(part);
-
-            //// updating part.
-            ccmbase_obj_name.update_collision(k_mer, round);
-
-        }
-    }
-}
+template <typename ccmbase_obj> void update_count_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name);
+template <typename ccmbase_obj> void update_collision_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name, int round);
+template <typename ccmbase_obj> void update_count_collision_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name, int round, int total_round);
 
 
 int main(){
 
-    sketch::ocm::ccmbase <int32_t, sketch::hash::HasherSet<sketch::hash::WangHash> > cms_obj(19,100,137,false);   // np = 5, nh = 10, seed = 137,
-    sketch::ocm::ccmbase <int32_t, sketch::hash::HasherSet<sketch::hash::WangHash> > ccms_obj(19,100,137,true);
+    sketch::ocm::ccmbase <int32_t, sketch::hash::HasherSet<sketch::hash::WangHash> > cms_obj(19,10,137,false);   // np = 5, nh = 10, seed = 137,
+    sketch::ocm::ccmbase <int32_t, sketch::hash::HasherSet<sketch::hash::WangHash> > ccms_obj(19,10,137,true);
 
     update_count_from_file("rymv.sim.fa",22, cms_obj);
     cout<<"True Count: "<<counter<<endl;
@@ -122,8 +28,8 @@ int main(){
 
 
     //construct OCM
-    sketch::ocm::ocmbase <uint64_t, sketch::hash::HasherSet<sketch::hash::WangHash> > sketch1(19,100,137);
-    for(int r = 1; r<= 3; r++){
+    sketch::ocm::ocmbase <uint64_t, sketch::hash::HasherSet<sketch::hash::WangHash> > sketch1(19,10,137);
+    for(int r = 1; r<= 10; r++){
         if (r > 1){
             // for all kmers update collision
             update_collision_from_file("rymv.sim.fa",22,sketch1,r);
@@ -135,6 +41,32 @@ int main(){
     //end construct OCM
 
     cout<<"Offline CMS Estimate count: "<<sketch1.est_count(cal(sample_kmer))<<endl;
+
+
+    //construct OCCM
+    sketch::ocm::ocmbase <uint64_t, sketch::hash::HasherSet<sketch::hash::WangHash> > sketch2(19,10,137);
+    for(int r = 1; r<= 10; r++){
+        if (r > 1){
+            // for all kmers update collision
+            update_collision_from_file("rymv.sim.fa",22,sketch2,r);
+        }
+        sketch2.clear_core();
+        // for all kmer update count.
+        update_count_collision_from_file("rymv.sim.fa",22,sketch2,r,10);
+    }
+    //end construct OCCM
+    cout<<"Offline CCMS Estimate count: "<<sketch2.est_count(cal(sample_kmer))<<endl;
+
+    //query using original data:
+    ifstream qifile("rymv.sim.22mer.counts.txt");
+    ofstream ofile("QueryOutput.txt");
+    string kmer_str; int file_count;
+    while(!qifile.eof()){
+        qifile>>kmer_str>>file_count;
+        uint64_t kmer_cal = cal(kmer_str);
+        ofile<<kmer_str<<": File-Count: "<<file_count<<" CMS-count: "<<cms_obj.est_count(kmer_cal)<<" CCMS-count: "<<ccms_obj.est_count(kmer_cal)<<" OCMS-count: "<<sketch1.est_count(kmer_cal)<<" OCCMS-count: "<<sketch2.est_count(kmer_cal)<<" \n";
+        cout<<kmer_str<<": File-Count: "<<file_count<<" CMS-count: "<<cms_obj.est_count(kmer_cal)<<" CCMS-count: "<<ccms_obj.est_count(kmer_cal)<<" OCMS-count: "<<sketch1.est_count(kmer_cal)<<" OCCMS-count: "<<sketch2.est_count(kmer_cal)<<" \n";
+    }
 }
 
 void my_binary(int64_t n) //converting k-mer to binary representation
@@ -193,4 +125,152 @@ int64_t cal(string str_k_mer)
     //my_binary(k_mer);
     return k_mer;
 
+}
+
+
+template <typename ccmbase_obj> void update_count_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name)
+{
+    std::ifstream input(file);
+    if(!input.good()){cout<<"Can't open"<<endl;}
+
+    std::string line, name, content;
+    while( std::getline( input, line ).good()){ //reading 1 line from input
+
+        if( line.empty() || line[0] == '>' ){
+            if( !name.empty() ){
+                int l=content.length();
+                for(int i=0; i<=l-len_k_mer; i++) //extracting k-mers from the line
+                {
+                    string part = content.substr(i, len_k_mer);
+                    int64_t k_mer = cal(part);
+                    /////////Updating part.
+                    ccmbase_obj_name.update_count(k_mer);
+                    if(k_mer == cal(sample_kmer))counter++;
+                }
+                name.clear();
+            }
+            if( !line.empty() ){name = line.substr(1);}
+            content.clear();
+        }
+        else if( !name.empty() ){
+            if( line.find(' ') != std::string::npos ){
+                name.clear();
+                content.clear();
+            }
+            else {
+                content += line;
+            }
+        }
+    }
+    if( !name.empty() ){
+        //std::cout << name << " : " << content << std::endl;
+        int l=content.length();
+        for(int i=0; i<=l-len_k_mer; i++)
+        {
+            string part = content.substr(i, len_k_mer);
+            int64_t k_mer = cal(part);
+
+            //// updating part.
+            ccmbase_obj_name.update_count(k_mer);
+            if(k_mer == cal(sample_kmer))counter++; //manual count of the sample k-mer
+
+        }
+    }
+}
+
+
+template <typename ccmbase_obj> void update_collision_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name, int round)
+{
+    std::ifstream input(file);
+    if(!input.good()){cout<<"Can't open"<<endl;}
+
+    std::string line, name, content;
+    while( std::getline( input, line ).good()){ //reading 1 line from input
+
+        if( line.empty() || line[0] == '>' ){
+            if( !name.empty() ){
+                int l=content.length();
+                for(int i=0; i<=l-len_k_mer; i++) //extracting k-mers from the line
+                {
+                    string part = content.substr(i, len_k_mer);
+                    int64_t k_mer = cal(part);
+                    /////////Updating part.
+                    ccmbase_obj_name.update_collision(k_mer, round);
+                }
+                name.clear();
+            }
+            if( !line.empty() ){name = line.substr(1);}
+            content.clear();
+        }
+        else if( !name.empty() ){
+            if( line.find(' ') != std::string::npos ){
+                name.clear();
+                content.clear();
+            }
+            else {
+                content += line;
+            }
+        }
+    }
+    if( !name.empty() ){
+        //std::cout << name << " : " << content << std::endl;
+        int l=content.length();
+        for(int i=0; i<=l-len_k_mer; i++)
+        {
+            string part = content.substr(i, len_k_mer);
+            int64_t k_mer = cal(part);
+
+            //// updating part.
+            ccmbase_obj_name.update_collision(k_mer, round);
+
+        }
+    }
+}
+
+template <typename ccmbase_obj> void update_count_collision_from_file(string file, int len_k_mer, ccmbase_obj &ccmbase_obj_name, int round, int total_round)
+{
+    std::ifstream input(file);
+    if(!input.good()){cout<<"Can't open"<<endl;}
+
+    std::string line, name, content;
+    while( std::getline( input, line ).good()){ //reading 1 line from input
+
+        if( line.empty() || line[0] == '>' ){
+            if( !name.empty() ){
+                int l=content.length();
+                for(int i=0; i<=l-len_k_mer; i++) //extracting k-mers from the line
+                {
+                    string part = content.substr(i, len_k_mer);
+                    int64_t k_mer = cal(part);
+                    /////////Updating part.
+                    ccmbase_obj_name.update_count_collision(k_mer, round, total_round);
+                }
+                name.clear();
+            }
+            if( !line.empty() ){name = line.substr(1);}
+            content.clear();
+        }
+        else if( !name.empty() ){
+            if( line.find(' ') != std::string::npos ){
+                name.clear();
+                content.clear();
+            }
+            else {
+                content += line;
+            }
+        }
+    }
+    if( !name.empty() ){
+        //std::cout << name << " : " << content << std::endl;
+        int l=content.length();
+        for(int i=0; i<=l-len_k_mer; i++)
+        {
+            string part = content.substr(i, len_k_mer);
+            int64_t k_mer = cal(part);
+
+            //// updating part.
+            ccmbase_obj_name.update_count_collision(k_mer, round, total_round);
+
+        }
+    }
 }

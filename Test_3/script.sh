b872cdd395386b5c2ea06f21bf4ab8b63577e8a1
Ashis Saha <alorchhota@gmail.com>
2021-11-11 15:02:38 -0800

    c++ standard option added in script.sh

--- Test_3/script.sh
+++ Test_3/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm ocm
-g++ main.cpp -o ocm
+g++ -std=c++17 -o ocm main.cpp
 ./ocm count -k 22 -h 7 -w 1048576 -n 4 -t 3 -o output.bin -fa rymv.sim.fa
 ./ocm query -f output.bin -q query.txt -o query_result.txt

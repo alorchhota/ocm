9378484967e6946e76950b70f8b5123fc78f326f
mmk <mmkdhrubo@gmail.com>
2022-01-01 16:25:05 +0600

    fixed folder structure

---
+++ output/analysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import math
+import sklearn.metrics
+df = pd.read_csv('query_result.csv')
+print(df.head())
+print("sparman correlation coffecient:")
+sp_corr = df.corr(method='spearman')['true_count'].drop('true_count')
+print(sp_corr)
+
+
+rms_error = math.sqrt(sklearn.metrics.mean_squared_error(df['true_count'], df['estimated_count']))
+print("RMS Error: ",rms_error)
